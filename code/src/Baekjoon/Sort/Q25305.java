5944330fd9391b172798fd6f3872d5d55857b114
KyoungWoong <kyy980708@gmail.com>
2023-07-19 00:42:00 +0900

    단계별로 풀기 (정렬)

---
+++ code/src/Baekjoon/Sort/Q25305.java
@@ -0,0 +1,24 @@
+package Baekjoon.Sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Q25305 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken()) - 1;
+        st = new StringTokenizer(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(list, Collections.reverseOrder());
+        System.out.println(list.get(K));
+    }
+}
