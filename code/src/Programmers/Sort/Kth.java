8c71fc2832beb31355d373e2dff29662b200bacf
Kyoungwoong <kyy980708@gmail.com>
2024-11-11 17:43:24 +0900

    프로그래머스 정렬

--- code/src/Programmers/Sort/Kth.java
+++ code/src/Programmers/Sort/Kth.java
@@ -9,6 +9,26 @@ public class Kth {
     private static int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}}; // [5,6,3]
 
     public static void main(String[] args) {
+//        prev();
+        int[] ans = nov11();
+    }
+
+    private static int[] nov11() {
+        List<Integer> answerList = new ArrayList<>();
+
+        for (int[] command: commands) {
+            int i = command[0] - 1;
+            int j = command[1];
+            int k = command[2] - 1;
+            int[] subArray = Arrays.copyOfRange(array, i, j);
+            Arrays.sort(subArray);
+            answerList.add(subArray[k]);
+        }
+
+        return answerList.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private static void prev() {
         List<Integer> answer = new ArrayList<>();
         for (int[] cur : commands) {
             int i = cur[0], j = cur[1], k = cur[2];
