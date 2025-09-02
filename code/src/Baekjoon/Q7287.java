bdf28f7afaace7f2d13927b35ab660a66073ccd0
Kyoungwoong <kyy980708@gmail.com>
2025-09-02 19:12:49 +0900

    baekjoon Q7287

---
+++ code/src/Baekjoon/Q7287.java
@@ -0,0 +1,9 @@
+package Baekjoon;
+
+import java.io.IOException;
+
+public class Q7287 {
+    public static void main(String[] args) throws IOException {
+        System.out.println(String.format("%d\n%s", 535, "kyy980708"));
+    }
+}
