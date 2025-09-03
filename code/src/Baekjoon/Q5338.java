e95ecd930c8eb9e6683a9f5b854883e0f1e354da
Kyoungwoong <kyy980708@gmail.com>
2025-09-03 22:00:16 +0900

    baekjoon q5338

---
+++ code/src/Baekjoon/Q5338.java
@@ -0,0 +1,15 @@
+package Baekjoon;
+
+public class Q5338 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%s\n%s\n%s\n%s\n%s",
+                "       _.-;;-._",
+                "'-..-'|   ||   |",
+                "'-..-'|_.-;;-._|",
+                "'-..-'|   ||   |",
+                "'-..-'|_.-''-._|"
+        ));
+        System.out.println(sb.toString());
+    }
+}
