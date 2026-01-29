97a8c6b47849bc5f352dae12c0ccee862cf8773a
Kyoungwoong <kyy980708@gmail.com>
2026-01-29 15:18:39 +0900

    Q5337

---
+++ code/src/Baekjoon/Q5337.java
@@ -0,0 +1,8 @@
+package Baekjoon;
+
+public class Q5337 {
+    public static void main(String[] args) {
+        System.out.println(String.format("%s\n%s\n%s",
+                ".  .   .", "|  | _ | _. _ ._ _  _", "|/\\|(/.|(_.(_)[ | )(/."));
+    }
+}
