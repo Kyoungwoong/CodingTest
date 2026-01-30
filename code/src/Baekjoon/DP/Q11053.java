22e60d1bdb095b76ef13133bf3d6b1f0b316b58e
Kyoungwoong <kyy980708@gmail.com>
2026-01-30 21:53:10 +0900

    Q11053

--- code/src/Baekjoon/DP/Q11053.java
+++ code/src/Baekjoon/DP/Q11053.java
@@ -3,15 +3,49 @@ package Baekjoon.DP;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.StringTokenizer;
 
+/**
+ * 수열 A가 주어졌을 때, 가장 긴 증가하는 부분 수열을 구하는 프로그램을 작성하시오.
+ *
+ * 예를 들어, 수열 A = {10, 20, 10, 30, 20, 50} 인 경우에 가장 긴 증가하는 부분 수열은 A = {10, 20, 10, 30, 20, 50} 이고, 길이는 4이다.
+ */
 public class Q11053 {
     private static int N;
     private static int[] arr, dp;
 
     public static void main(String[] args) throws IOException {
 //        prev();
-        april16();
+//        april16();
+        jan29();
+    }
+
+    private static void jan29() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        dp = new int[N + 1];
+        Arrays.fill(dp, 1);
+        dp[0] = 0;
+        int ans = -1;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (arr[j - 1] < arr[i - 1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+            System.out.print(dp[i] + " ");
+        }
+        System.out.println();
+        System.out.println(ans);
     }
 
     private static void april16() throws IOException {
