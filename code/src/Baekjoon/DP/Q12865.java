b1c2e0b8a8b32b13db4b2ac37a289fd5878aecbd
Kyoungwoong <kyy980708@gmail.com>
2026-01-30 21:53:47 +0900

    Q12865

--- code/src/Baekjoon/DP/Q12865.java
+++ code/src/Baekjoon/DP/Q12865.java
@@ -8,112 +8,57 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
+/**
+ * 준서가 여행에 필요하다고 생각하는 N개의 물건이 있다.
+ * 각 물건은 무게 W와 가치 V를 가지는데, 해당 물건을 배낭에 넣어서 가면 준서가 V만큼 즐길 수 있다.
+ * 아직 행군을 해본 적이 없는 준서는 최대 K만큼의 무게만을 넣을 수 있는 배낭만 들고 다닐 수 있다.
+ * 준서가 최대한 즐거운 여행을 하기 위해 배낭에 넣을 수 있는 물건들의 가치의 최댓값을 알려주자.
+ */
 public class Q12865 {
-    static class BackPack {
-        int weight, value;
 
-        public BackPack(int weight, int value) {
-            this.weight = weight;
-            this.value = value;
-        }
-    }
     private static int N, K;
-    private static int[] W = new int[101];
-    private static int[] V = new int[101];
-    private static List<BackPack> backPacks = new ArrayList<>();
+    private static int[] weights, values;
 
     public static void main(String[] args) throws IOException {
-//        prev();
-        may4();
-
-    }
-    private static void may4() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
+        weights = new int[N];
+        values = new int[N];
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            backPacks.add(new BackPack(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+            weights[i] = Integer.parseInt(st.nextToken());
+            values[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] dp = new int[100001];
-        Arrays.fill(dp, -1);
-        dp[0] = 0;
+        // dp의 열은 무게.
+        // dp의 행은 i번째 보석까지 고려함(i번째 보석을 고려함, 고려하지 않음)
+        int[] dp = new int[K + 1];
         for (int i = 0; i < N; i++) {
-            int w = backPacks.get(i).weight;
-            int v = backPacks.get(i).value;
+            int w = weights[i], v = values[i];
             for (int j = K; j >= w; j--) {
-                if (j - w < 0) {
-                    continue;
-                }
-                if (dp[j - w] == -1) {
-                    continue;
-                }
                 dp[j] = Math.max(dp[j], dp[j - w] + v);
             }
         }
-
-        int ans = -1;
-        for (int i = 0; i <= K; i++) {
-            ans = Math.max(ans, dp[i]);
-        }
-        System.out.println(ans);
-    }
-
-    private static void prev() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int w = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            W[i] = w;
-            V[i] = v;
-        }
-
-        // i번째 보석까지 고려했을 때 j 무게일때의 최대 가치
-        int[] dp = new int[100001];
-        int MIN = Integer.MIN_VALUE;
-        Arrays.fill(dp, MIN);
-
-        int w = W[0];
-        int v = V[0];
-        if (w <= K) {
-            dp[w] = Math.max(dp[w], v);
-        }
-        dp[0] = 0;
-
-        for (int i = 1; i < N; i++) {
-            w = W[i];
-            v = V[i];
-
-            for (int j = K; j >= w; j--) {
-                if (j - w < 0) {
-                    continue;
-                }
-                if(dp[j-w] == MIN) {
-                    continue;
-                }
-                dp[j] = Math.max(dp[j], dp[j-w] + v);
-            }
-
-//            for (int q = 0; q <= K; q++) {
-//                System.out.print(dp[q] + " ");
+//        for (int i = 0; i <= N; i++) {
+//            for (int j = 0; j <= K; j++) {
+//                System.out.print(dp[i][j] + " ");
 //            }
 //            System.out.println();
-        }
-
-        int ans = MIN;
-        for (int i = 0; i <= K; i++) {
-//            System.out.print(dp[i] + " ");
-            ans = Math.max(ans, dp[i]);
-        }
-//        System.out.println();
-        System.out.println(ans);
+//        }
+        System.out.println(dp[K]);
     }
 }
+
+/*
+4 7
+6 13
+4 8
+3 6
+5 12
+---
+14
+ */
