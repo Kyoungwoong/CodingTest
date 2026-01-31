8ef874d2dd02cc7728adc2baa198152ef44847c6
Kyoungwoong <kyy980708@gmail.com>
2026-01-31 14:11:22 +0900

    Q1463

--- code/src/Baekjoon/DP/Q1463.java
+++ code/src/Baekjoon/DP/Q1463.java
@@ -3,6 +3,7 @@ package Baekjoon.DP;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Q1463 {
     private static int N;
@@ -20,7 +21,42 @@ public class Q1463 {
 
     public static void main(String[] args) throws IOException {
 //        prev();
-        april16();
+//        april16();
+        jan29();
+    }
+
+    private static final int INVALID = Integer.MAX_VALUE;
+    private static void jan29() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int[] array = new int[X + 1];
+        Arrays.fill(array, INVALID);
+        array[1] = 0;
+        if (X >= 2) {
+            array[2] = 1;
+            if (X >= 3) {
+                array[3] = 1;
+            }
+        }
+
+        for (int i = 4; i <= X; i++) {
+            // X가 3으로 나누어 떨어지면, 3으로 나눈다.
+            if (i % 3 == 0 && array[i / 3] != INVALID) {
+                array[i] = Math.min(array[i], array[i / 3] + 1);
+            }
+            // X가 2으로 나누어 떨어지면, 2으로 나눈다.
+            if (i % 2 == 0 && array[i / 2] != INVALID) {
+                array[i] = Math.min(array[i], array[i / 2] + 1);
+            }
+            // 1을 뺀다
+            array[i] = Math.min(array[i], array[i - 1] + 1);
+        }
+
+//        for (int i = 0; i <= X; i++) {
+//            System.out.print(array[i] + "\t");
+//        }
+//        System.out.println();
+        System.out.println(array[X]);
     }
 
     private static void april16() throws IOException {
