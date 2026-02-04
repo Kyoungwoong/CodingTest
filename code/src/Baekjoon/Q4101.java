2f4b2aa4c6be26da04efb5703b91119a1f6570b8
Kyoungwoong <kyy980708@gmail.com>
2026-02-05 00:25:05 +0900

    Q4101

---
+++ code/src/Baekjoon/Q4101.java
@@ -0,0 +1,27 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q4101 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0) {
+                break;
+            }
+
+            sb.append(a > b ? "Yes" : "No").append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
