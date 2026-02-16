64a93e56ca41a60315d40ca3bb30e83d0b0601aa
Kyoungwoong <kyy980708@gmail.com>
2026-02-16 23:11:25 +0900

    baekjoon Q16394

---
+++ code/src/Baekjoon/Q16394.java
@@ -0,0 +1,16 @@
+package Baekjoon;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Q16394 {
+    static final int OPEN = 1946;
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        System.out.println((N - OPEN));
+    }
+}
