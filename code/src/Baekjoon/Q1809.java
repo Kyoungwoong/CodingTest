a0580b9c0546bc6f478450b5ee542b809099413f
Kyoungwoong <kyy980708@gmail.com>
2026-02-16 23:10:38 +0900

    baekjoon Q1809

---
+++ code/src/Baekjoon/Q1809.java
@@ -0,0 +1,11 @@
+package Baekjoon;
+
+public class Q1809 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%s\n%s\n%s\n%s\n%s\n%s",
+                "(___)", "(o o)____/", " @@      \\", "  \\ ____,/", "   //   //", "  ^^   ^^"));
+        System.out.println(sb.toString());
+
+    }
+}
