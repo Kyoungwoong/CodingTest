f6d8dc28234bd163b9a81f442d8a81e7c029b2ef
Kyoungwoong <kyy980708@gmail.com>
2026-02-17 17:13:27 +0900

    baekjoon dp Q2133

---
+++ code/src/Baekjoon/DP/Q2133.java
@@ -0,0 +1,23 @@
+package Baekjoon.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2133 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+        dp[0] = 1;
+        if (N >= 2) dp[2] = 3;
+        for (int i = 4; i <= N; i += 2) {
+            dp[i] = dp[i - 2] * 3;
+            for (int k = 4; k <= i; k += 2) {
+                dp[i] += dp[i - k] * 2;
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
