40e88f391819f50efb532616633ca39136b5b2bc
Kyoungwoong <kyy980708@gmail.com>
2026-02-20 22:47:54 +0900

    NH programmers sql virus

---
+++ code/src/NH/SQL/SELECT/virus.sql
@@ -0,0 +1,19 @@
+with recursive cte as (
+    -- anchor
+    select id, parent_id, 1 as generation
+    from ecoli_data
+    where parent_id is null
+
+    union all
+
+    -- recursive
+    select c2.id, c2.parent_id, c1.generation+1 as generation
+    from cte c1 inner join ecoli_data c2
+                           on c1.id = c2.parent_id
+)
+
+select
+    cte.id
+from cte
+where generation =3
+order by cte.id;
