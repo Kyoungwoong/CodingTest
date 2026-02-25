b8dad17575d80a98595b1ab03de720e139a3c125
Kyoungwoong <kyy980708@gmail.com>
2026-02-25 16:42:31 +0900

    baekjoon Q15680

---
+++ code/src/Baekjoon/Q15680.java
@@ -0,0 +1,12 @@
+package Baekjoon;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Q15680 {
+    public static void main(String[] args) throws IOException {
+        int N = new Scanner(System.in).nextInt();
+
+        System.out.println(N == 0 ? "YONSEI" : "Leading the Way to the Future");
+    }
+}
